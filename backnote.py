524f1d09672b10de83de7247aa39adcbcd6334c8
yuuri <houjie95@foxmail.com>
2020-04-27 16:28:04 +0800

    add backnote

---
+++ backnote.py
@@ -0,0 +1,13 @@
+
+# 1.use one user get her orgs
+# 2.get orgs info
+# 2.1 get orgs user
+# 3. get org's repo
+# 4.get repo branch
+# 5.get repo release
+# 6.get repo tags
+# 7.get repo user
+# 8.get repo commit
+# get repo commit file
+
+OGR_URL = 'https://api.github.com/users/liewegas/orgs'
